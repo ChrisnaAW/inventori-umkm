acda01e578ca2d41b1e7031f210e0bdde7b4b7e8
ChrisnaAW <chrisnaarya98@gmail.com>
2026-07-05 09:32:26 +0700

    update CORS production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://inventori-umkm.vercel.app/"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 require("./config/database");
 
